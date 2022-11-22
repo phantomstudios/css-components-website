0671876d773881f10e01c6e5d233b2d30a9f2f92
John Richard Chipps-Harding <john.chipps-harding@phntms.com>
2022-11-22 10:46:06 +0000

    WIP (#4)
    * WIP
    
    * component composing done
    
    * typos

---
+++ src/components/MDXComponents/CheckoutButton/styles.ts
@@ -0,0 +1,8 @@
+import { styled } from "@phntms/css-components";
+
+import css from "./styles.module.css";
+import DemoButton from "../DemoButton";
+
+export const CheckoutButtonWrapper = styled(DemoButton, {
+  css: css.CheckoutButtonWrapper,
+});
