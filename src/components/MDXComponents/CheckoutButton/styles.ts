b9a59fee4543dc62ff8ab0be794258a449e60134
John Richard Chipps-Harding <john.chipps-harding@phntms.com>
2022-11-22 10:29:06 +0000

    component composing done

--- src/components/MDXComponents/CheckoutButton/styles.ts
+++ src/components/MDXComponents/CheckoutButton/styles.ts
@@ -1,35 +1,8 @@
 import { styled } from "@phntms/css-components";
 
 import css from "./styles.module.css";
+import DemoButton from "../DemoButton";
 
-export const CheckoutButtonWrapper = styled("button", {
-  css: css.DemoButtonWrapper,
-  // variants: {
-  //   color: {
-  //     violet: css.colorViolet,
-  //     gray: css.colorGray,
-  //   },
-  //   size: {
-  //     small: css.sizeSmall,
-  //     large: css.sizeLarge,
-  //   },
-  //   outlined: {
-  //     true: css.outlined,
-  //   },
-  // },
-  // compoundVariants: [
-  //   {
-  //     color: "violet",
-  //     outlined: true,
-  //     css: css.colorOutlinedViolet,
-  //   },
-  //   {
-  //     color: "gray",
-  //     outlined: true,
-  //     css: css.colorOutlinedGray,
-  //   },
-  // ],
-  // defaultVariants: {
-  //   color: "violet",
-  // },
+export const CheckoutButtonWrapper = styled(DemoButton, {
+  css: css.CheckoutButtonWrapper,
 });
