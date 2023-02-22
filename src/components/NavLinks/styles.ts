38f22fc6de459e6761d8e354fb69ebf126c4b8b4
George <georgegloyens@gmail.com>
2023-02-22 12:18:58 +0000

    TS formatting

--- src/components/NavLinks/styles.ts
+++ src/components/NavLinks/styles.ts
@@ -3,14 +3,14 @@ import { styled } from "@phntms/css-components";
 import css from "./styles.module.css";
 
 export const Docs = styled("ul", {
-    css: css.Docs,
+  css: css.Docs,
 });
 
 export const List = styled("li", {
-    css: css.List,
-    variants: {
-        active: {
-            true: css.active,
-        },
+  css: css.List,
+  variants: {
+    active: {
+      true: css.active,
     },
+  },
 });
