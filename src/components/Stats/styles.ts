372168c5614c8df0aea19e070adcc0b87ee7c62c
George <georgegloyens@gmail.com>
2023-02-23 11:17:31 +0000

    Refactor statsWrapper to use CSS Components instead of plain CSS

--- src/components/Stats/styles.ts
+++ src/components/Stats/styles.ts
@@ -5,3 +5,11 @@ import css from "./styles.module.css";
 export const StatsWrapper = styled("div", {
   css: css.statsWrapper,
 });
+
+export const StatsCards = styled("ul", {
+  css: css.statsCards,
+});
+
+export const StatCard = styled("li", {
+  css: css.statCard,
+});
