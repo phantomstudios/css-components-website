70a6dd1c51adacc5f3e84a0fdb9b6196a8bbd72e
Balraj Johal <balraj.johal@phntms.com>
2023-02-28 11:22:06 +0000

    Test dynamic title

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,11 +1,5 @@
-import type { Metadata } from "next";
-
 import DocsNav from "@/components/DocsNav";
 import DocsContents from "@/components/DocsContents";
-
-export const metadata: Metadata = {
-  title: "Documentation",
-};
 
 export default function RootLayout({
   children,
