7801587e66f147c3290bbb608346c27760e91a53
Balraj Johal <balraj.johal@phntms.com>
2023-02-28 11:55:38 +0000

    Test loader on build

---
+++ src/app/docs/[slug]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <h1 style={{ paddingLeft: "200px", color: "black", fontSize: "10em" }}>
+      LOADING
+    </h1>
+  );
+}
