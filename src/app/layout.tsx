7182b46b073f719b5bb4ca22f16b783eab2bdcfa
George <georgegloyens@gmail.com>
2023-02-28 16:10:41 +0000

    Add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,19 @@ export const metadata: Metadata = {
     description:
       "Not another styling system, but a lightweight utility to compose CSS styles into standard React.JS components.",
     url: "https://www.css-components.net",
+    images: [
+      {
+        url: "https://www.css-components.net/banner.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "CSS Components",
     creator: "@phntmLDN",
+    images: ["https://www.css-components.net/banner.png"],
   },
 };
 
