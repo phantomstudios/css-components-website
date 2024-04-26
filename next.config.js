26b9abaf77a2fac60bbd3fb9a8a263fe52fc7607
George Gloyens <george.gloyens@phntms.com>
2024-04-26 10:08:09 +0100

    Update documentation - phntms to phantomstudios

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverActions: true,
-    serverComponentsExternalPackages: ['mdx-bundler'],
+    serverComponentsExternalPackages: ["mdx-bundler"],
   },
-}
+};
 
 module.exports = nextConfig;
